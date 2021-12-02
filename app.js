2a8d823ca7a6937408aff680a5a8c3b490e38911
Will-Andrade <willian.santos.andra@outlook.com>
2021-12-02 01:08:38 -0400

    refactoring on the add todo function

--- app.js
+++ app.js
@@ -1,46 +1,63 @@
-//! Primeiro, adicionar todos
+// TODO: Refactoring. Entender cada parte do código e criar funções únicas
+// TODO: Implementar nova remoção de todo usando data- e dataset
 const formAddTodo = document.querySelector('.form-add-todo');
 const formSearchTodo = document.querySelector('.form-search input');
 const todosContainer = document.querySelector('.todos-container');
 
-formAddTodo.addEventListener('submit', e => {
+const createTodo = inputValue => todosContainer.innerHTML += `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${inputValue}</span>
+        <i class="far fa-trash-alt delete"></i>
+    </li>
+`;
+
+const resetForm = form => form.reset();
+
+const validateInputValue = e => {
     e.preventDefault();
 
     const inputValue = e.target.add.value.trim();
 
     if (inputValue.length) {
-        todosContainer.innerHTML += `
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            <span>${inputValue}</span>
-            <i class="far fa-trash-alt delete"></i>
-        </li>
-        `;
-
-        e.target.reset();
+        createTodo(inputValue);
+        resetForm(e.target);
     }
-});
+};
 
-//! Segundo, remover todos
-todosContainer.addEventListener('click', e => {
+const deleteTodoHandler = e => {
+    //! Verifica se o usuário clicou no ícone de lixeira
     const clickedOnTrash = Array.from(e.target.classList).includes('delete');
+    
+    //! Se clicou, o todo pai da lixeira é excluído
     clickedOnTrash ? e.target.parentElement.remove() : null;
-})
+};
 
-//! Terceiro, buscar/filtrar todos
-formSearchTodo.addEventListener('input', e => {
+const searchTodoHandler = e => {
+    //! transforma o valor do input
     const inputValue = e.target.value.trim().toLowerCase();
 
+    //! Cria um array dos todos do container
     Array.from(todosContainer.children)
+    //! Filtra todos não compatíveis comparando com o input value
         .filter(todo => !todo.textContent.trim().toLowerCase().includes(inputValue))
+    //! Cada todo que não conter o valor dentro do input é excluído
         .forEach(todo => {
             todo.classList.remove('d-flex');
             todo.classList.add('hidden');
         })
     
+    //! Cria um array dos todos do container
     Array.from(todosContainer.children)
+    //! Filtra todos compatíveis comparando com o input value
         .filter(todo => todo.textContent.trim().toLowerCase().includes(inputValue))
+    //! Cada todo que conter o valor dentro do input será colocado na tela
         .forEach(todo => {
             todo.classList.remove('hidden');
             todo.classList.add('d-flex');
         })
-})
+};
+
+formAddTodo.addEventListener('submit', validateInputValue);
+todosContainer.addEventListener('click', deleteTodoHandler);
+formSearchTodo.addEventListener('input', searchTodoHandler);
+ 
